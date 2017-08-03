1e7be8fc41ac69aa81c62d6c04c871cc50902625
fangtao04 <fangtao04@meituan.com>
2017-08-03 21:30:51 +0800

    koa-router

---
+++ app2.js
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import path from 'path'
+import css from 'css'
+import Koa from 'koa'
+import koaBody from 'koa-body'
+import Router from 'koa-router'
+import fetch from 'node-fetch'
+
+const app = new Koa()
+const router = new Router()
+
+app.use(koaBody())
+
+router.get('/', async(ctx, next) => {
+    ctx.body = fs.readFileSync('./view/index.html').toString()
+})
+
+router.post('/', async(ctx, next) => {
+    const {url} = ctx.request.body
+    const data = await fetch(url).then(rs => rs.text())
+
+    const cssText = css.stringify(css.parse(data), {
+        indent: '  ',
+        column: 4,
+        compress: false
+    })
+
+    ctx.body = cssText
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(3000)

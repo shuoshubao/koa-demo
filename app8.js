e8ddd5a3de42ef3bdf42cf3d61a597afdec5ea7a
fangtao04 <fangtao04@meituan.com>
2017-08-04 19:19:28 +0800

    get article

---
+++ app8.js
@@ -0,0 +1,55 @@
+import fs from 'fs'
+import path from 'path'
+// import querystring from 'querystring'
+const querystring = require('querystring')
+import chalk from 'chalk'
+import ejs from 'ejs'
+import Koa from 'koa'
+import koaBody from 'koa-body'
+import Router from 'koa-router'
+
+import articleJSON from './data/article.json'
+
+const app = new Koa()
+const router = new Router()
+
+app.use(koaBody())
+
+const readFile = path => new Promise((resolve, reject) => {
+    fs.readFile(path, (e, data) => {
+        if(e) {
+            reject(e)
+        }else {
+            resolve(data.toString())
+        }
+    })
+})
+
+router
+.get('/', async(ctx, next) => {
+    const str = await readFile('./view/app8.ejs')
+    const ret = ejs.render(str, {
+        articleJSON
+    })
+    ctx.body = ret
+})
+.get('/docs/:category/:title', async(ctx, next) => {
+    const url = path.resolve(__dirname, 'docs', `${ctx.params.category}/${ctx.params.title}.md`)
+    const str = await readFile(url)
+    ctx.body = str
+})
+.get('/api/getArticleList', async(ctx, next) => {
+    const params = querystring.parse(ctx.querystring)
+    ctx.body = articleJSON[params.category]
+})
+.post('/docs/:category/:title', async(ctx, next) => {
+    const url = path.resolve(__dirname, 'docs', `${ctx.params.category}/${ctx.params.title}.md`)
+    const ret = await readFile(url)
+    ctx.body = ret
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(3000)
